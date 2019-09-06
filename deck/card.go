566844aa88b01153e3fb090196530dc4b0851b8d
dlokkers <33578368+dlokkers@users.noreply.github.com>
2019-09-06 08:41:52 -0400

    New deck and initial sorting helpers

--- deck/card.go
+++ deck/card.go
@@ -7,7 +7,7 @@ import "fmt"
 // Suit of the card
 type Suit uint8
 
-// Available suits of the cards
+// Suits available
 const (
 	Spade Suit = iota
 	Diamond
@@ -16,10 +16,12 @@ const (
 	Joker
 )
 
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
+
 // Rank of the cards
 type Rank uint8
 
-// Available ranks of the cards
+// Ranks available
 const (
 	_ Rank = iota
 	Ace
@@ -37,15 +39,50 @@ const (
 	King
 )
 
+var ranks = [...]Rank{Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King}
+
 // Card is the representation of each card in a deck
 type Card struct {
 	Suit
 	Rank
 }
 
+// Cards represents a deck or cards+
+type Cards []Card
+
 func (c Card) String() string {
 	if c.Suit == Joker {
 		return "Joker"
 	}
 	return fmt.Sprintf("%s of %ss", c.Rank, c.Suit)
+}
+
+// New creates a new deck of cards, including set of optional
+// parameters
+func New() []Card {
+	var cards []Card
+
+	for _, s := range suits {
+		for _, r := range ranks {
+			cards = append(cards, Card{s, r})
+		}
+	}
+
+	return cards
+}
+
+// Len defines the size of the deck, to implement Sort
+func (c Cards) Len() int {
+	return len(c)
+}
+
+// Swap defines a means to swap cards for sorting purposes
+func (c Cards) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+// Less defines an ordering in cards for sorting purposes
+func (c Cards) Less(i, j int) bool {
+	return int(c[i].Suit)*len(ranks)+int(c[j].Rank) <
+		int(c[j].Suit)*len(ranks)+int(c[j].Rank)
 }
